3bedc58a74284e4b477811edd98c5f55c672c238
Nathan <nathan.walter@ynov.com>
2025-05-22 23:39:42 +0200

    Adding a scene playground + fixing speed and jump to be more like Valorant or CSGO

--- Assets/Scripts/CapsuleController.cs
+++ Assets/Scripts/CapsuleController.cs
@@ -4,10 +4,10 @@ using UnityEngine.InputSystem;
 public class CapsuleController : MonoBehaviour
 {
     [Header("Déplacement")]
-    public float moveSpeed = 5f;
-    public float moveWalk = 2f;
+    public float runSpeed = 5f;
+    public float walkSpeed = 2f;
     public float jumpForce = 5f;
-    public float gravity = -9.81f;
+    public float gravity = -40f;  // Gravité augmentée pour éviter flottement
 
     [SerializeField] private CharacterController controller;
     [SerializeField] private Vector3 velocity;
@@ -15,7 +15,6 @@ public class CapsuleController : MonoBehaviour
     [SerializeField] private Vector2 moveInput;
     [SerializeField] private bool jumpQueued;
     [SerializeField] private bool isWalking = false;
-
 
     private void Awake()
     {
@@ -28,26 +27,25 @@ public class CapsuleController : MonoBehaviour
 
         if (isGrounded && velocity.y < 0)
         {
-            velocity.y = -2f;
+            velocity.y = -2f;  // Pour coller le joueur au sol
         }
 
         Vector3 move = transform.right * moveInput.x + transform.forward * moveInput.y;
+        float currentSpeed = isWalking ? walkSpeed : runSpeed;
+        controller.Move(move * currentSpeed * Time.deltaTime);
 
-        if (isWalking)
-        {
-            controller.Move(move * moveWalk * Time.deltaTime);
-        }
-        else
-        {
-            controller.Move(move * moveSpeed * Time.deltaTime);
-
-        }
         if (jumpQueued && isGrounded)
         {
             velocity.y = Mathf.Sqrt(jumpForce * -2f * gravity);
             jumpQueued = false;
         }
-        velocity.y += gravity * Time.deltaTime;
+
+        // Une seule application de la gravité
+        if (velocity.y < 0)
+            velocity.y += gravity * 1.5f * Time.deltaTime;
+        else
+            velocity.y += gravity * Time.deltaTime;
+
         controller.Move(velocity * Time.deltaTime);
     }
 
@@ -63,9 +61,10 @@ public class CapsuleController : MonoBehaviour
             jumpQueued = true;
         }
     }
+
     public void WalkRun(InputAction.CallbackContext context)
     {
-        if (context.performed && isGrounded)
+        if (context.performed)
         {
             isWalking = !isWalking;
         }
