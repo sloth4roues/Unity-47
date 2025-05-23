6e65fd65ebdf5f0687276b155e5680f43fb6a7be
Nathan <nathan.walter@ynov.com>
2025-05-23 20:30:23 +0200

    Fixing the UI bug with OrbUI

--- Assets/Scripts/OrbCollector.cs
+++ Assets/Scripts/OrbCollector.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -5,6 +6,18 @@ public class OrbCollector : MonoBehaviour
 {
     public float interactRange = 2f;
     public LayerMask OrbLayer;
+
+    [Header("UltOrb")]
+    public int maxOrb = 20;
+    public int currentOrb = 0;
+
+    public static event Action<int, int> OnOrbChanged;
+
+    void Start()
+    {
+        currentOrb = 0;
+        OnOrbChanged?.Invoke(currentOrb, maxOrb);
+    }
 
     public void Interact(InputAction.CallbackContext context)
     {
@@ -16,15 +29,21 @@ public class OrbCollector : MonoBehaviour
 
     void TryCollectOrb()
     {
-        Collider[] hits = Physics.OverlapSphere(transform.position, interactRange);
+        if (currentOrb >= maxOrb)
+            return;
+
+        Collider[] hits = Physics.OverlapSphere(transform.position, interactRange, OrbLayer);
 
         foreach (var hit in hits)
         {
             if (hit.CompareTag("Orb"))
             {
+                currentOrb++;
+                OnOrbChanged?.Invoke(currentOrb, maxOrb); // MAJ UI
                 Destroy(hit.gameObject);
                 return;
             }
         }
     }
+
 }
---
+++ Assets/Scripts/OrbUI.cs
@@ -0,0 +1,23 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OrbUI : MonoBehaviour
+{
+    public TextMeshProUGUI orbText;
+
+    void OnEnable()
+    {
+        OrbCollector.OnOrbChanged += UpdateOrb;
+    }
+
+    void OnDisable()
+    {
+        OrbCollector.OnOrbChanged -= UpdateOrb;
+    }
+
+    void UpdateOrb(int current, int max)
+    {
+        orbText.text = $"Ult{current}/{max}";
+    }
+}
