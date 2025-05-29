1f1aaea5baaaabb4aef75c65776274384b83c1c2
Nathan <nathan.walter@ynov.com>
2025-05-29 03:49:47 +0200

    Fixing Layers issue with Craft, Hit Effect fully working

--- Assets/Scripts/HitEffectManager.cs
+++ Assets/Scripts/HitEffectManager.cs
@@ -3,30 +3,33 @@ using UnityEngine;
 public class HitEffectManager : MonoBehaviour
 {
     public GameObject targetHitEffect;
-    public GameObject defaultHitEffect;
+    public GameObject craftHitEffect;
 
     public LayerMask hitLayer;
     public LayerMask craftLayer;
 
     public void SpawnHitEffect(RaycastHit hit)
     {
-        Debug.Log("HitEffect déclenché sur : " + hit.collider.name);
+
         GameObject effectToSpawn = null;
 
-        if (((1 << hit.collider.gameObject.layer) & hitLayer) != 0)
+        int hitObjectLayer = hit.collider.gameObject.layer;
+        int hitLayerMask = 1 << hitObjectLayer;
+
+        if ((hitLayerMask & hitLayer) != 0)
         {
             effectToSpawn = targetHitEffect;
         }
-        else if (((1 << hit.collider.gameObject.layer) & craftLayer) != 0)
+        else if ((hitLayerMask & craftLayer) != 0)
         {
-            effectToSpawn = defaultHitEffect;
+            effectToSpawn = craftHitEffect;
         }
 
         if (effectToSpawn != null)
         {
             GameObject spawned = Instantiate(effectToSpawn, hit.point, Quaternion.LookRotation(hit.normal));
             spawned.transform.SetParent(null);
-            Destroy(spawned, 1.5f); // Nettoyage au bout de 1.5f
+            Destroy(spawned, 1.5f);
         }
     }
 }
